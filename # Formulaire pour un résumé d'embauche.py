3c2dc2d0a156f8cbe504daab4fb510b8f81d69f5
HonestYann <HonestYann@users.noreply.github.com>
2025-11-18 13:01:46 -0500

    Formulaire Yann

---
+++ # Formulaire pour un résumé d'embauche.py
@@ -0,0 +1,10 @@
+# Formulaire pour un résumé d'embauche
+
+print("Bienvenue au formulaire d'embauche.")
+nom=input("Veuillez entrer votre nom complet : ")
+age=input("Veuillez entrer votre âge : ")
+experience=input("Veuillez décrire votre expérience professionnelle : ")
+competences=input("Veuillez lister vos compétences principales : ")
+print("\Résumé d'embauche")
+print(age,experience,competences,nom )    
+print("merci pour votre candidature,", nom+". Nous examinerons votre profil et vous contacterons bientôt.")
